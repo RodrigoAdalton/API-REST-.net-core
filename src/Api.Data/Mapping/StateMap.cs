4f8d0b3966a63935667613f7668ad7ecea23c9a4
Rodrigo Adalton <rodrigoadalton@hotmail.com>
2022-08-07 17:23:02 -0300

    address data for mapping entities

---
+++ src/Api.Data/Mapping/StateMap.cs
@@ -0,0 +1,19 @@
+using Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Api.Data.Mapping
+{
+    public class StateMap : IEntityTypeConfiguration<StateEntity>
+    {
+        public void Configure(EntityTypeBuilder<StateEntity> builder)
+        {
+            builder.ToTable("State");
+
+            builder.HasKey(u => u.Id);
+
+            builder.HasIndex(u => u.Initial)
+                   .IsUnique();
+        }
+    }
+}
